7e610b4c3909845cb5125fa05699eed572284295
Blake Lassiter <blake.lassiter@microsoft.com>
2024-03-28 11:30:06 -0700

    sync all nuget packages to latest

--- src/ApiService/ApiService.cs
+++ src/ApiService/ApiService.cs
@@ -4,7 +4,13 @@
 
 namespace Microsoft.Purview.DataGovernance.Provisioning.ApiService;
 
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Purview.DataGovernance.Common;
 using Microsoft.Purview.DataGovernance.EventHub;
+using Microsoft.Purview.DataGovernance.EventHub.Configuration;
+using Microsoft.Purview.DataGovernance.Loggers;
+using System.Configuration;
 
 /// <summary>
 /// Extension methods for configuring the Api Service.
@@ -18,7 +24,13 @@ public static class ApiService
     public static IServiceCollection AddApiServices(
         this IServiceCollection services)
     {
-        services.AddSingleton<IEventPublisher, EventHubPublisher>();
+        services.AddSingleton<IEventPublisher>(services => 
+        {
+            IServiceRequestLogger logger = services.GetService<IServiceRequestLogger>();
+            IOptions<EventHubPublisherConfiguration> configuration = services.GetService<IOptions<EventHubPublisherConfiguration>>();
+            AzureCredentialFactory credentialFactory = services.GetService<AzureCredentialFactory>();
+            return new EventHubPublisher(logger, configuration.Value, credentialFactory);
+        });
 
         return services;
     }
